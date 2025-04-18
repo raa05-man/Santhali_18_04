acf59afdd746f920c1dca88d5e44deff81ed2568
Raman Kumar <187097814+raa05-man@users.noreply.github.com>
2025-04-18 16:53:18 +0530

    modified header

--- santhali_frontend/src/components/Header_1/Header.jsx
+++ santhali_frontend/src/components/Header_1/Header.jsx
@@ -1,8 +1,10 @@
 import "./Header.css" ;
+import React from "react";
 
 export default function Header(){
     return(
         <div>
+            <i className="fa-solid fa-bars"></i>
             <h1>Santhali Translator</h1>
             <div className="logo">
             <img src="src/assets/jharkhand_logo.png" alt="Image" />
