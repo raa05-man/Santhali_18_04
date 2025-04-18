13caaf9972aae501dddd891de91842e7332f016d
ShubhamPandey121 <shubham230884@gmail.com>
2025-04-18 17:03:57 +0530

    Added Santali Info

--- santhali_frontend/src/App.jsx
+++ santhali_frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from '../header'
 import Translator from './components/Translator/Translator'
+import SantaliInfo from './components/SantaliInfo/SantaliInfo'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
       <Header/>
         <Translator />
+        <SantaliInfo/>
 
     </>
   )
