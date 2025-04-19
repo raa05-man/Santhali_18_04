21405f745d7f644d235a34530e25b84e5d7fdfcf
Raman Kumar <187097814+raa05-man@users.noreply.github.com>
2025-04-19 10:34:15 +0530

    modified header

--- santhali_frontend/src/components/Header_1/Header.jsx
+++ santhali_frontend/src/components/Header_1/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     density_medium
   </span>
       <h1 className="text-[#000000]/40% text-2xl sm:text-3xl md:text-4xl font-semibold text-center font-[Hanken_Grotesk] absolute top-20">
-        Santhali Translator
+        Santali Translator
       </h1>
 
       <div className="flex justify-center items-center gap-8 mt-4 flex-wrap absolute top-30">
